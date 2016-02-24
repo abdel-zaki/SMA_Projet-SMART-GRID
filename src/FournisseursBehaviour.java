a0d62d5d9186c8bae153a8c56fabfc14e064c30a
Abdelwahed ZAKI <abdel1109@gmail.com>
2016-02-24 20:09:21 +0100

    Update FournisseursBehaviour.java
    Behaviour de l'agent environnement
    - donner le feu vert à l'agent horloge

--- src/FournisseursBehaviour.java
+++ src/FournisseursBehaviour.java
@@ -1,6 +1,7 @@
 import jade.core.AID;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+
 import jade.lang.acl.UnreadableException;
 import java.io.IOException;
 import java.util.HashMap;
